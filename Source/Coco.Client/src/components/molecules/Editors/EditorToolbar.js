321a6f59b1e2b97c6e781c510d5262889ac78132
trungleit <trungle.it@gmail.com>
2020-02-10 21:44:58 +0700

    Implement add link to text for Draft Editor

--- Source/Coco.Client/src/components/molecules/Editors/EditorToolbar.js
+++ Source/Coco.Client/src/components/molecules/Editors/EditorToolbar.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
 import { EditorState, Modifier, SelectionState } from "draft-js";
+import { getEntityRange, getSelectionEntity } from "draftjs-utils";
 import { DefaultButton } from "./EditorButtons";
 import EditorDropdown from "./EditorDropdown";
 
@@ -138,6 +139,99 @@ export default props => {
     props.clearFormat(newEditorState);
   };
 
+  const addLink = (linkTitle, linkTarget, linkTargetOption) => {
+    const currentEntity = editorState
+      ? getSelectionEntity(editorState)
+      : undefined;
+    let selection = editorState.getSelection();
+
+    if (currentEntity) {
+      const entityRange = getEntityRange(editorState, currentEntity);
+      const isBackward = selection.getIsBackward();
+      if (isBackward) {
+        selection = selection.merge({
+          anchorOffset: entityRange.end,
+          focusOffset: entityRange.start
+        });
+      } else {
+        selection = selection.merge({
+          anchorOffset: entityRange.start,
+          focusOffset: entityRange.end
+        });
+      }
+    }
+
+    const entityKey = editorState
+      .getCurrentContent()
+      .createEntity("LINK", "MUTABLE", {
+        url: linkTarget,
+        targetOption: linkTargetOption
+      })
+      .getLastCreatedEntityKey();
+
+    let contentState = Modifier.replaceText(
+      editorState.getCurrentContent(),
+      selection,
+      `${linkTitle}`,
+      editorState.getCurrentInlineStyle(),
+      entityKey
+    );
+
+    let newEditorState = EditorState.push(
+      editorState,
+      contentState,
+      "insert-characters"
+    );
+
+    // insert a blank space after link
+    selection = newEditorState.getSelection().merge({
+      anchorOffset: selection.get("anchorOffset") + linkTitle.length,
+      focusOffset: selection.get("anchorOffset") + linkTitle.length
+    });
+    newEditorState = EditorState.acceptSelection(newEditorState, selection);
+    contentState = Modifier.insertText(
+      newEditorState.getCurrentContent(),
+      selection,
+      " ",
+      newEditorState.getCurrentInlineStyle(),
+      undefined
+    );
+
+    props.onAddLink({
+      newEditorState,
+      entityKey: "insert-characters"
+    });
+    // onChange(
+    //   EditorState.push(newEditorState, contentState, 'insert-characters')
+    // );
+    // this.doCollapse();
+  };
+
+  const onAddLink = () => {
+    const link = window.prompt("Paste the link -");
+    addLink("link title", link, {});
+
+    // const contentState = editorState.getCurrentContent();
+    // const contentStateWithEntity = contentState.createEntity(
+    //   "LINK",
+    //   "MUTABLE",
+    //   { url: link }
+    // );
+    // const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
+    // const newEditorState = EditorState.set(editorState, {
+    //   currentContent: contentStateWithEntity
+    // });
+
+    // props.onAddLink({
+    //   newEditorState,
+    //   entityKey
+    // });
+  };
+
+  const onRemoveLink = e => {
+    props.onRemoveLink(e);
+  };
+
   useEffect(() => {
     return () => {
       return clearTimeout();
@@ -168,6 +262,8 @@ export default props => {
         />
       ))}
       <Divide />
+      <EditorButton icon="link" onToggle={onAddLink} />
+      <EditorButton icon="unlink" onToggle={onRemoveLink} />
       <SelectHeading
         options={headingTypes}
         actived={blockType}
