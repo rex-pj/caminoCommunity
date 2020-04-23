7d4521e8b41e521020dafca64a86c29bbd07e2f1
trungleit <trungle.it@gmail.com>
2020-04-23 20:36:02 +0700

    Begin implement admin page

---
+++ Source/Coco.Server/Coco.Management/Controllers/ProductCategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Coco.Management.Controllers
+{
+    public class ProductCategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
---
+++ Source/Coco.Server/Coco.Management/ViewComponents/NavAccordionViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Coco.Management.ViewComponents
+{
+    public class NavAccordionViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
