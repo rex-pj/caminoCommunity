fa0112e04efefe183b835e714201ba1a2247a47c
trungleit <trungle.it@gmail.com>
2020-07-29 22:26:20 +0700

    Fix login failed after setup

--- src/caminoSocial/run.bat
+++
@@ -1,5 +0,0 @@
-start dotnet run --project ./Api.Identity/Api.Identity.csproj
-start dotnet run --project ./Api.Content/Api.Content.csproj
-start dotnet run --project ./Api.Public/Api.Public.csproj
-start dotnet run --project ./Api.Resources/Api.Resources.csproj
-start dotnet run --project ./Api.Gateway/Api.Gateway.csproj
